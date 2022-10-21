951d5f8038a4666c3c4a9e81fef523f95744af96
DenEH <91687628+deniss-eh@users.noreply.github.com>
2022-10-21 13:44:20 +0100

    .

--- Java/Ternary.java
+++ Java/Ternary.java
@@ -1,5 +1,3 @@
-// Online Java Compiler
-// Use this editor to write, compile and run your Java code online
 import java.util.Scanner;
 class HelloWorld {
     public static void main(String[] args) {
